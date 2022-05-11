c785b303329d84f980ae9d3726629fa0cc403e0a
FernandoRuizParietti <ferdyjrp@yahoo.com.ar>
2022-05-11 18:27:27 -0300

    second commit

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[]= [];
+  heroeSeleccionado: Heroe | undefined
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
+  buscando(){
+    this.heroesService.getSugerencias(this.termino.trim()) //trim quita los espacios antes y despues del termino
+    .subscribe(heroes => this.heroes = heroes)
+  }
+
+  opcionSeleccionada(event:MatAutocompleteSelectedEvent ){
+
+    if(!event.option.value){
+      console.log('no hay valor');
+      //con esto limpio la busqueda anterior si se ingresa un terino equivocado
+      this.heroeSeleccionado= undefined 
+      return
+    }
+
+    const heroe: Heroe = event.option.value
+    this.termino= heroe.superhero  
+    
+    this.heroesService.getHeroeById(heroe.id!)
+    .subscribe(heroe => this.heroeSeleccionado = heroe)
+  }
 }
